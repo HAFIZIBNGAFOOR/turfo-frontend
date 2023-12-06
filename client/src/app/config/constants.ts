8ca4a874cef5a6f6ca28a5b3349cbf9e9d8769aa
HAFIZIBNGAFOOR <hafizahmed0303@gmail.com>
2023-12-06 16:54:33 +0530

    after production build

--- client/src/app/config/constants.ts
+++ client/src/app/config/constants.ts
@@ -3,7 +3,7 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class Constants{
 
-    public readonly UserAPIEndPoint:string = 'https://turfo.online/';
+    public readonly UserAPIEndPoint:string = 'https://turfo.online';
     public readonly AdminAPIEndPoint:string = 'https://turfo.online/admin';
     public readonly TurfOwnerAPIEndPoint:string = 'https://turfo.online/turfAdmin';
 
