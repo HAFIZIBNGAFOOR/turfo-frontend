3dac56fa590f12301d47da702a27c01c53d8369f
HAFIZIBNGAFOOR <hafizahmed0303@gmail.com>
2023-12-09 11:57:32 +0530

    changed api

--- client/src/app/config/constants.ts
+++ client/src/app/config/constants.ts
@@ -3,11 +3,9 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class Constants{
 
-    // public readonly UserAPIEndPoint:string = 'https://turfo.online';
-    // public readonly AdminAPIEndPoint:string = 'https://turfo.online/admin';
-    // public readonly TurfOwnerAPIEndPoint:string = 'https://turfo.online/turfAdmin';
-    public readonly UserAPIEndPoint:string = 'http://localhost:3001';
-    public readonly AdminAPIEndPoint:string = 'http://localhost:3001/admin';
-    public readonly TurfOwnerAPIEndPoint:string = 'http://localhost:3001/turfAdmin';
+    public readonly UserAPIEndPoint:string = 'https://turfo.online';
+    public readonly AdminAPIEndPoint:string = 'https://turfo.online/admin';
+    public readonly TurfOwnerAPIEndPoint:string = 'https://turfo.online/turfAdmin';
+
 
 }
